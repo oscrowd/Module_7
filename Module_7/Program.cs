6b2a2ae8a6bba22c54a36bd4281c50a2eafddee3
oscrowd <oscrowd@gmail.com>
2023-03-27 23:47:34 +0300

    D-01.000.01 BaseLavel

--- Module_7/Program.cs
+++ Module_7/Program.cs
@@ -4,6 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+abstract class Delivery
+{
+    public string Address;
+}
+
+class HomeDelivery : Delivery
+{
+    /* ... */
+}
+
+class PickPointDelivery : Delivery
+{
+    /* ... */
+}
+
+class ShopDelivery : Delivery
+{
+    /* ... */
+}
+
+public class Price
+{ 
+
+}
+
+public class ProductProperties
+{ 
+}
+
+
+class Order<TDelivery,
+TStruct> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+
+    public int Number;
+
+    public string Description;
+
+    public Price price;
+
+    //Композиция с Price
+    public Order()
+    {
+        price = new Price();            
+    }
+
+
+    private string weight;
+
+    public string Weight
+    {
+        get
+        {
+            return weight;
+        }
+        set 
+        { 
+            weight = value;
+        }
+    }
+
+
+
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+
+    // ... Другие поля
+}
+
 namespace Module_7
 {
     internal class Program
