31c56430c62bcd92dac0f7bd0e6bbff56b46b1c2
oscrowd <oscrowd@gmail.com>
2023-03-29 20:40:12 +0300

    D-01.000.02 BaseLavel

--- Module_7/Program.cs
+++ Module_7/Program.cs
@@ -25,9 +25,29 @@ class ShopDelivery : Delivery
 }
 
 public class Price
-{ 
+{
+    public virtual int Cost
+    {
+        get;
+        set;
+    }
+
+    public void PrintPrice()
+    {
+        Console.WriteLine("Цена: {0}", Cost);
+    }
+}
+
+public class Costing : Price
+{
+    public int cost;
+    public override int Cost 
+    { 
+        get => base.Cost; set => base.Cost = value; 
+        }
 
 }
+
 
 public class ProductProperties
 { 
@@ -38,23 +58,28 @@ class Order<TDelivery,
 TStruct> where TDelivery : Delivery
 {
     public TDelivery Delivery;
-
     public int Number;
-
     public string Description;
+    public Price price; //Композиция с Price
+    private double weight;
 
-    public Price price;
-
-    //Композиция с Price
+    //Композиция с Price, Конструктор
     public Order()
     {
         price = new Price();            
     }
 
+    
 
-    private string weight;
-
-    public string Weight
+    //Конструктор
+    public Order(int N, string D, double w) 
+    {
+        Number = N;
+        Description = D;
+        Weight = w; 
+    }    
+            
+    public double Weight
     {
         get
         {
@@ -65,9 +90,7 @@ TStruct> where TDelivery : Delivery
             weight = value;
         }
     }
-
-
-
+    
     public void DisplayAddress()
     {
         Console.WriteLine(Delivery.Address);
